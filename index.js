c5188678cdcd55210701de71b887089539aa3a68
SCHT001 <sachetsubedi001@gmail.com>
2024-01-27 22:48:41 +0545

    server change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
-// const socket = io("https://chat-cli-1587.onrender.com");
-const socket=io("http://localhost:3000")
+const socket = io("https://chat-cli-1587.onrender.com");
+// const socket=io("http://localhost:3000")
 // socket.emit('message','hello from the other side');
 
 // create connection
