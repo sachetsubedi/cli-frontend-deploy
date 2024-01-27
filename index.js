cea6347eb2a5d044ce8f1a5b4afb6520dd3ec4c0
SCHT001 <sachetsubedi001@gmail.com>
2024-01-27 21:57:40 +0545

    remote server added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
-const socket = io("http://localhost:3000");
+const socket = io("https://chat-cli-1587.onrender.com");
 // socket.emit('message','hello from the other side');
 
 // create connection
