369337df01fe5591cad0224208d10159e74fc328
Sachet Subedi <sachetsubedi001@gmail.com>
2024-04-07 20:59:56 +0545

    migrated to render

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const socket = io(`https://cli-backend.sachetsubedi001.com.np`);
-// const socket = io("http://localhost:3000");
+// const socket = io(`https://cli-backend.sachetsubedi001.com.np`);
+const socket = io("http://localhost:3000");
 let userId;
 let roomId = "master";
 
